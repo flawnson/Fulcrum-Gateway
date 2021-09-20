74af1a6da7b3b97b5cf56b7a8ac5eab67a50fe54
flawnson <flawnsontong1@gmail.com>
2021-09-20 10:06:44 -0400

    Reset project and reinstalled dependencies to working version

---
+++ fulcrum_gateway/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NativeBaseProvider, Box } from 'native-base';
+
+function App() {
+  return (
+      <NativeBaseProvider>
+        <Box>Hello world</Box>
+      </NativeBaseProvider>
+  );
+}
+
+export default App
