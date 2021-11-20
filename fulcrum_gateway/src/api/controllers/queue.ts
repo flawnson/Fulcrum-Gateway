ce56ad5b9276066792fbb0065979db665ac831be
Kevin Shen <kshen3778@gmail.com>
2021-11-20 17:26:57 -0500

    Added queue statistics to schema

--- fulcrum_gateway/src/api/controllers/queue.ts
+++ fulcrum_gateway/src/api/controllers/queue.ts
@@ -21,6 +21,11 @@ export const typeDef = `
     """The state of the queue: paused, active"""
     state: QueueState!,
     average_wait_time: Float,
+    num_enqueued: Int!,
+    num_serviced: Int!,
+    num_deferred: Int!,
+    num_abandoned: Int!,
+    num_noshows: Int!,
     enqueued: [User]!,
     serviced: [User]!,
     deferred: [User]!,
@@ -31,7 +36,20 @@ export const typeDef = `
 
 export const resolvers = {
   Query: {
-    //TODO
+    queue(){
+      //calculate statistics
+      const stats = {
+        num_enqueued: 37,
+        num_serviced: 23,
+        num_deferrals: 8,
+        average_wait_time: 12.5,
+        num_abandoned: 2,
+        num_noshows: 0
+      }
+
+      
+
+    }
   },
   Queue: {
     //resolve queue fields
