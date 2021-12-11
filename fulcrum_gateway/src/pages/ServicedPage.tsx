006fc67e89eff406b5df84ffc5a57c57c3d73f9c
flawnson <flawnsontong1@gmail.com>
2021-12-11 10:04:11 -0500

    Corrected queries to prisma/typegql format, continued work on frontend features

--- fulcrum_gateway/src/pages/ServicedPage.tsx
+++ fulcrum_gateway/src/pages/ServicedPage.tsx
@@ -1,8 +1,14 @@
-import React, { SetStateAction, useEffect, useState } from "react";
+import React, {SetStateAction, useEffect, useState} from "react";
 import useInterval from "../utilities/useInterval";
 import ServicedCatalogCardGroup from "../components/molecules/ServicedCatalogCardGroup";
-import { ServicedStats } from "../../types";
+import {EnqueuedStats} from "../../types";
 
+
+type ServicedStats = {
+    userId: number,
+    name: string,
+    reneged: number,
+}
 
 export default function () {
     const [props, setProps] = useState<ServicedStats[]>([])
@@ -34,7 +40,7 @@ export default function () {
             await response.json().then(
                 data => {
                     data = data.data.queue.users
-                    data = data.filter((d: ServicedStats) => d.state === "SERVICED")
+                    data = data.filter((d: EnqueuedStats) => d.state === "SERVICED")
                     let serviced_stats: ServicedStats[] = []
                     data.forEach((serviced_data: any) => {
                         const join_time: any = new Date(serviced_data.join_time)
