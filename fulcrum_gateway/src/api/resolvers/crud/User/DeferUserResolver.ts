73dfc586f6916fe9927001458d7f303daf9897e3
Kevin Shen <kshen3778@gmail.com>
2022-01-05 19:59:53 -0500

    update deferuser

--- fulcrum_gateway/src/api/resolvers/crud/User/DeferUserResolver.ts
+++ fulcrum_gateway/src/api/resolvers/crud/User/DeferUserResolver.ts
@@ -4,12 +4,14 @@ import {
   Root, Int,
   Mutation, Arg, Args, ArgsType,
   InputType, Field,
-  Authorized
+  Authorized, UseMiddleware
 } from "type-graphql";
 import { User } from "../../../../../prisma/generated/type-graphql/models/User";
 import { Context } from "../../../context.interface";
 import * as helpers from "../../../helpers";
 import _ from 'lodash'
+import { userAccessPermission } from "../../../middleware/userAccessPermission";
+
 
 @ArgsType()
 class DeferUserArgs {
@@ -28,64 +30,24 @@ class DeferUserArgs {
 export class DeferUserResolver {
 
   @Authorized()
+  @UseMiddleware(userAccessPermission)
   @Mutation(returns => User, {
     nullable: true
   })
   async deferPosition(@Ctx() ctx: Context, @Args() args: DeferUserArgs): Promise<User | null> {
 
-    // if accessed by organizer
-    if (ctx.req.session.queueId && args.userId){
-      const exists = await helpers.userExistsInQueue(args.userId, ctx.req.session.queueId);
-      if (exists === false){
-        return null;
-      }
-      const userIndex = await ctx.prisma.user.findUnique({
-        where: {
-          id: ctx.req.session.userId,
-        },
-        select: {
-          index: true
-        }
-      });
-      // const queue = prisma.queue.findUnique({
-      //   where: {
-      //     id: req.session.queueId,
-      //   }
-      // });
-      // const users = prisma.user.findMany({
-      //   orderBy: {
-      //     index: 'desc'
-      //   },
-      //   include: {
-      //     estimated_wait:
-      //   }
-      // })
-      // const subsequentUsers = _.filter(users, user => user.index > userIndex)
-      // const sumSubsequentUsers = _.sum()
+    let queryUserId = "";
 
+    if (ctx.req.session!.userId) {
+      queryUserId = ctx.req.session!.userId;
     }
 
-    // if accessed by user
-    if (ctx.req.session.userId){
-      // TODO: defer logic
+    if (args.userId) {
+      queryUserId = args.userId;
     }
 
-    return null;
-
-
-    // May need to import CustomUserResolver to access estimated_wait()
-
-    // const new_wait = await this.estimated_wait(user, {prisma})
-    // const result = await prisma.user.update({
-    //   where: {
-    //     id: user.id
-    //   },
-    //   data: {
-    //     state: "DEFERRED",
-    //     estimated_wait: new_wait
-    //   }
-    // })
-    // return result
+    // Note to flawnson: Use queryUserId as the user id
+    // ...TODO
 
   }
 }
