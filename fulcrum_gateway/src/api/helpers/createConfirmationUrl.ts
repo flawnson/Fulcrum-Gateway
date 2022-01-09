b8a2001a2b954539d908286149d732ae00fca2e0
Kevin Shen <kshen3778@gmail.com>
2022-01-08 19:59:06 -0500

    fixed CORS origin url, prod vs local frontend url

--- fulcrum_gateway/src/api/helpers/createConfirmationUrl.ts
+++ fulcrum_gateway/src/api/helpers/createConfirmationUrl.ts
@@ -11,9 +11,9 @@ export const createConfirmationUrl = async (organizerId: string) => {
     // TODO: FILL THIS IN
     // RETURN PRODUCTION URL
     //for now return the same localhost url
-    return `http://localhost:8080/organizer/confirm/${confirmOrganizerPrefix + token}`;
+    return `${process.env.FRONTEND_PROD_URL}/organizer/confirm/${confirmOrganizerPrefix + token}`;
   }
 
   //development
-  return `http://localhost:8080/organizer/confirm/${confirmOrganizerPrefix + token}`; //fill in with correct URL
+  return `${process.env.FRONTEND_LOCAL_URL}/organizer/confirm/${confirmOrganizerPrefix + token}`; //fill in with correct URL
 };
