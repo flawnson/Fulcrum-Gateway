27747368053216b0e5cfee3b0c639388ca240906
Kevin Shen <kshen3778@gmail.com>
2022-01-27 10:03:13 -0500

    change user status is atomic

--- fulcrum/api/redisClient.ts
+++ fulcrum/api/redisClient.ts
@@ -4,5 +4,5 @@
 
 import Redis from "ioredis";
 //export const redis = new Redis(process.env.REDIS_URL);
-console.log("Connecting to Redis at port " + process.env.REDIS_PORT + " and ip " + process.env.REDIS_IP);
+console.log("Redis connection at port " + process.env.REDIS_PORT + " and ip " + process.env.REDIS_IP);
 export const redis = new Redis(Number(String(process.env.REDIS_PORT)), process.env.REDIS_IP);
