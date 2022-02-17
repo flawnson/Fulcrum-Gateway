d63b3381122fd72b803db07021b755c2a205a19d
Kevin Shen <kshen3778@gmail.com>
2022-02-17 16:25:42 -0500

    Readded Twilio API

--- fulcrum/api/helpers/sendSMS.ts
+++ fulcrum/api/helpers/sendSMS.ts
@@ -1,9 +1,20 @@
 import { Twilio } from "twilio";
 import initMB from 'messagebird';
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+let twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 let messageBirdToken = process.env.MESSAGEBIRD_TOKEN;
+
+let accountSid = process.env.TWILIO_ACCOUNT_SID;
+let authToken = process.env.TWILIO_AUTH_TOKEN;
+
+if(process.env.NODE_ENV === "development"){
+  accountSid = process.env.TWILIO_ACCOUNT_SID_TEST;
+  authToken = process.env.TWILIO_AUTH_TOKEN_TEST;
+}
+
+if(process.env.NODE_ENV === "development"){
+  messageBirdToken = process.env.MESSAGEBIRD_TOKEN_TEST;
+}
+
 const client = new Twilio(accountSid!, authToken!);
 const messagebird = initMB(messageBirdToken!);
 
@@ -13,35 +24,37 @@ export async function sendSMS(phoneNumber: string, message: string, task: string
     phoneNumber = "+" + phoneNumber;
   }
 
-  if(process.env.NODE_ENV === "test"){
-    messageBirdToken = process.env.MESSAGEBIRD_TOKEN_TEST;
-  }
-
-  if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test"){
-    const params = {
-      'originator': process.env.SMS_NUMBER,
-      'recipients': [
-        phoneNumber
-      ],
-      'body': message
-    };
-
-    // @ts-ignore
-    messagebird.messages.create(params, function (err, response) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(response);
-    });
-    // client.messages.create({
-    //    body: message,
-    //    from: twilioNumber,
-    //    to: phoneNumber
-    //  })
-    // .then(message => console.log(task + " SMS sent: " + message.sid));
-  }
-  else {
+  if(process.env.NODE_ENV === "development"){
+    //test phone number and twilio (won't actually charge)
+    twilioNumber = "+15005550006";
     console.log("(SMS dev mode) " + message);
   }
+
+
+  // //Messagebird API (backup)
+  // const params = {
+  //   'originator': process.env.SMS_NUMBER,
+  //   'recipients': [
+  //     phoneNumber
+  //   ],
+  //   'body': message
+  // };
+  //
+  // // @ts-ignore
+  // messagebird.messages.create(params, function (err, response) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   console.log(response);
+  // });
+  //
+
+  // Twilio API
+  client.messages.create({
+     body: message,
+     from: twilioNumber,
+     to: phoneNumber
+   })
+  .then(message => console.log(message.sid));
 
 }
