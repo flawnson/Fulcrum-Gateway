b981d1ce0eb9ffd9ee5efaf4b63989fba8429c98
Kevin Shen <kshen3778@gmail.com>
2022-02-17 09:37:01 -0500

    Added Rate limiter (#71)
    * Fixed user resolver integer errors
    
    * Added MessageBird api
    
    * Fixed bugs
    
    * fixed errors
    
    * Added rate limiter for backend

---
+++ fulcrum/api/middleware/rateLimiter.ts
@@ -0,0 +1,11 @@
+import rateLimit from 'express-rate-limit';
+import { errors } from "../constants";
+
+export const rateLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+  max: 20000, //20000 request allowed (to take into account for polling)
+  message: {
+    error: errors.REQUEST_LIMIT_EXCEEDED
+  },
+  headers: true,
+});
